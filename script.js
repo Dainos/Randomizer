3f6021d9075daca66baadd41acbfedbdf5397fda
Dainos <cauton777@gmail.com>
2019-08-29 12:48:53 +0300

    Реализован вывод сообщения об ошибке в поле для результата

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 let countBtn = document.getElementById("count");
+    res = document.getElementById("res");
 
 countBtn.addEventListener("click", function() {
     let min = +document.getElementById("min").value;
     let max = +document.getElementById("max").value;   
     if (min > max) {
-        alert("Ошибка! \nМаксимальное значение меньше минимального.");
+        res.textContent = "Ошибка! Максимальное значение меньше минимального.";
         return;
     }    
     let rand = Math.round(Math.random() * (max-min) + min);
-    document.getElementById("res").textContent = rand;
+    res.textContent = rand;
 });
 
 
